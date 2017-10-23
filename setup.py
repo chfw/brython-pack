b747eb5d2af5c817cae7affa2c459370c38bd6e7
chfw <wangc_2011@hotmail.com>
2017-10-23 21:57:35 +0100

    :egg: :ferris_wheel: release 0.0.2, that won't need gease for the end user

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PY26 = PY2 and sys.version_info[1] < 7
 
 NAME = 'brython-pack'
 AUTHOR = 'C.W.'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 EMAIL = 'wangc_2011@hotmail.com'
 LICENSE = 'MIT'
 ENTRY_POINTS = {
@@ -22,7 +22,7 @@ DESCRIPTION = (
     ''
 )
 URL = 'https://github.com/chfw/brython-pack'
-DOWNLOAD_URL = '%s/archive/0.0.1.tar.gz' % URL
+DOWNLOAD_URL = '%s/archive/0.0.2.tar.gz' % URL
 FILES = ['README.rst',  'CHANGELOG.rst']
 KEYWORDS = [
     'python'
@@ -51,8 +51,8 @@ EXTRAS_REQUIRE = {}
 # You do not need to read beyond this line
 PUBLISH_COMMAND = '{0} setup.py sdist bdist_wheel upload -r pypi'.format(
     sys.executable)
-GS_COMMAND = ('gs brython-pack v0.0.1 ' +
-              "Find 0.0.1 in changelog for more details")
+GS_COMMAND = ('gs brython-pack v0.0.2 ' +
+              "Find 0.0.2 in changelog for more details")
 NO_GS_MESSAGE = ('Automatic github release is disabled. ' +
                  'Please install gease to enable it.')
 UPLOAD_FAILED_MSG = ('Upload failed. please run "%s" yourself.')
