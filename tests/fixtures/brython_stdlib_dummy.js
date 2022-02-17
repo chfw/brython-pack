cf09640cf723fb9b1511eb9237f250c587881630
Adam Griffiths <adam.lw.griffiths@gmail.com>
2022-02-17 17:15:21 +1100

    Fix #2 by trimming the stdlib to between both { }, not just {.
    Brython's stdlib changed format since this was written and now
    contains extra data at the end of the JSON data.
    This means that trimming the contents of this file to just the
    leading { is not enough, we must also find the closing } and
    trim to that.
    Updated the dummy stdlib file in tests.
    With the fix in place, tests once again pass with Brython 3.9.5.

--- tests/fixtures/brython_stdlib_dummy.js
+++ tests/fixtures/brython_stdlib_dummy.js
@@ -1,4 +1,6 @@
-__BRYTHON__ = {
+__BRYTHON__.use_VFS = true;
+var scripts = {
   "testmodule": [".py", "print(\"hello world\")"],
   "testjs": [".js", "console.log(\"hello world\");"]
 }
+__BRYTHON__.update_VFS(scripts)
